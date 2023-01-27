2e66ab86984a1d9e41a6624d5635c6b6952d9ac1
Son Truong <sntruong12@gmail.com>
2023-01-27 14:52:46 -0800

    recursive length checker

--- chapters/04/quicksort.go
+++ chapters/04/quicksort.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	start := time.Now()
 
-	exercise4_1()
+	// exercise4_1()
+	exercise4_2()
 
 	duration := time.Since(start)
 	fmt.Printf("Elapsed Time: %.5fms\n", duration.Seconds()*1000)
@@ -29,5 +30,19 @@ func recursiveSumOfArray(arr []int) int {
 		return 0
 	} else {
 		return arr[0] + recursiveSumOfArray(arr[1:])
+	}
+}
+
+// Write a recursive funciton to count the number of items in a list
+func exercise4_2() {
+	count := recursiveCountItemsInList([]int{1, 23, 43, 455, 513, 862, 7544})
+	fmt.Println(count)
+}
+
+func recursiveCountItemsInList(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	} else {
+		return 1 + recursiveCountItemsInList(arr[1:])
 	}
 }
