2cd0f5642cf4ddcb0b2073d73e5b9c84e1e4f58e
Son Truong <sntruong12@gmail.com>
2023-01-27 15:21:29 -0800

    find max number recursively

--- chapters/04/quicksort.go
+++ chapters/04/quicksort.go
@@ -10,7 +10,8 @@ func main() {
 	start := time.Now()
 
 	// exercise4_1()
-	exercise4_2()
+	// exercise4_2()
+	exercise4_3()
 
 	duration := time.Since(start)
 	fmt.Printf("Elapsed Time: %.5fms\n", duration.Seconds()*1000)
@@ -45,4 +46,38 @@ func recursiveCountItemsInList(arr []int) int {
 	} else {
 		return 1 + recursiveCountItemsInList(arr[1:])
 	}
+}
+
+func exercise4_3() {
+	max := betterFindMaxNumber([]int{123, 23, 1231, 455, 5557, 863, 23, 5557, 551, 60, 790}, 0)
+	fmt.Println(max)
+	// max = findMaxNumber([]int{})
+	// fmt.Println(max)
+	// max = findMaxNumber([]int{123})
+	// fmt.Println(max)
+	// max = findMaxNumber([]int{12, 3})
+	// fmt.Println(max)
+}
+
+func findMaxNumber(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	} else if len(arr) == 1 {
+		return arr[0]
+	} else if arr[0] > arr[1] {
+		return findMaxNumber(append(arr[:1], arr[2:]...)) // remove the 1 index in arry
+	} else {
+		return findMaxNumber(arr[1:]) // remove the 0 index in array
+	}
+}
+
+// better way of writing it
+func betterFindMaxNumber(arr []int, max int) int {
+	if len(arr) == 0 {
+		return max
+	}
+	if arr[0] > max {
+		max = arr[0]
+	}
+	return betterFindMaxNumber(arr[1:], max)
 }
