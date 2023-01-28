079f1cb22b12ef0d978d3b50ba47933e81e44646
Son Truong <sntruong12@gmail.com>
2023-01-27 16:01:26 -0800

    quick sort

--- chapters/04/quicksort.go
+++ chapters/04/quicksort.go
@@ -84,3 +84,52 @@ func betterFindMaxNumber(arr []int, max int) int {
 
 // base case of binary search is if list is zero or one, then we know not to continue a recursive function as we know the answer
 // recursive case is based on what we are searching for, if it's greater than or less than the search query then we will continue the search function on the truthy case meaning first half or second half.
+
+// base case for quick sort is empty array or array with 1 element
+// arrays with 2 elements easy to quick sort
+// check if first element is smaller than the second
+// if it isn't then swap them
+
+// quick sort inner workings
+// select an element from array - this is called the pivot
+// now find all elements smaller than pivot
+// then find all elements larger than pivot
+// this is called partitioning
+// you have sub array with all smaller values than pivot
+// and the same for larger values than pivot
+// note at this point the sub arrays are not sorted
+
+// how do you sort the sub arrays?
+// call quicksort recursively on the two sub arrays
+// append left + pivot + right to have sorted array
+
+// ChaptGPT response
+// QuickSort is a divide and conquer sorting algorithm that uses a pivot element to partition the array into smaller sub-arrays, and then recursively sorts each sub-array. The pivot element is chosen as the last element of the array in this example.
+
+// The basic steps of the QuickSort algorithm are as follows:
+
+// Select an element in the array as the pivot element.
+// Partition the array into two sub-arrays, one with elements less than or equal to the pivot and one with elements greater than the pivot.
+// Recursively sort the sub-array of elements less than or equal to the pivot.
+// Recursively sort the sub-array of elements greater than the pivot.
+// Merge the sorted sub-arrays and the pivot to form the final sorted array.
+
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	pivot := arr[len(arr)-1]
+	left := make([]int, 0, len(arr))
+	right := make([]int, 0, len(arr))
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] <= pivot {
+			left = append(left, arr[i])
+		} else {
+			right = append(right, arr[i])
+		}
+	}
+
+	return append(append(quickSort(left), pivot), quickSort(right)...)
+}
