90d5f537ae1f94d8a96f54d148438d76b582e20e
Son Truong <sntruong12@gmail.com>
2023-01-31 11:53:33 -0800

    notes on breadth-first algo

--- chapters/06/breadthfirstsearch.go
+++ chapters/06/breadthfirstsearch.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+// breadth-first search allows you to find shortest distance beween two things
+// it is algorithm used on graphs
+
+// a shortest-path problem can be solved using the breadth-first search algorithm
+
+// graphs are made up of nodes and edges
+// node being a point
+// an edge linking nodes to nodes - neighboring nodes as you can call it (nodes that share an edge)
+// graphs are a way to model how different things are connected to one another
+
+// this algo helps answer two types questions
+// 1. is there a path from node A to node B?
+// 2. what's the shortest path from node A to node B?
+
+// queues
+
 func main() {
 	start := time.Now()
 
