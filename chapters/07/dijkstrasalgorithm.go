d468a032e2aefb0e62e88b380d7f301ab52828cb
Son Truong <sntruong12@gmail.com>
2023-02-06 18:37:44 -0800

    initialize example exercise

--- chapters/07/dijkstrasalgorithm.go
+++ chapters/07/dijkstrasalgorithm.go
@@ -38,13 +38,20 @@ func main() {
 }
 
 func dijkstras() {
-	graph := map[string]map[string]int{
-		"start": {
-			"A": 6,
-			"B": 2,
-		},
-	}
+	graph := map[string]map[string]int{}
 
-	fmt.Println(graph["start"]["A"])
-	fmt.Println(graph["start"]["B"])
+	graph["start"] = map[string]int{}
+	graph["start"]["a"] = 6
+	graph["start"]["b"] = 2
+
+	graph["a"] = map[string]int{}
+	graph["a"]["fin"] = 1
+
+	graph["b"] = map[string]int{}
+	graph["b"]["a"] = 3
+	graph["b"]["fin"] = 5
+
+	graph["fin"] = map[string]int{}
+
+	fmt.Println(graph)
 }
